13ca87305db543bd90f17c1b6001fd44928a9865
IzzyCode <awesome@email.com>
2019-02-25 15:49:48 -0500

    Fix indentation.

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ fetch('https://swapi.co/api/people')
     .catch( errors => console.log( errors ) )
   })
 
-  characters.addEventListener("change", e => {
-    results.innerHTML = ""
-    let charAttributes = ["name", 'height', "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender"]
+characters.addEventListener("change", e => {
+  results.innerHTML = ""
+  let charAttributes = ["name", 'height', "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender"]
 
-    console.log("changing", e);
-    let selectedChar = charactersFromApi[ Number(e.target.value) ]
-    let dl = document.createElement("dl")
-    charAttributes.forEach( attr => {
-      let dt = document.createElement("dt")
-      let dd = document.createElement("dd")
-      dt.textContent = attr
-      dt.style.textTransform = "capitalize"
-      dd.textContent = selectedChar[attr]
-      dl.appendChild(dt)
-      dl.appendChild(dd)
-    })
-    console.log(dl);
-    results.appendChild(dl)
-    results.classList.toggle("fadeIn")
-    results.classList.remove("d-none")
+  console.log("changing", e);
+  let selectedChar = charactersFromApi[ Number(e.target.value) ]
+  let dl = document.createElement("dl")
+  charAttributes.forEach( attr => {
+    let dt = document.createElement("dt")
+    let dd = document.createElement("dd")
+    dt.textContent = attr
+    dt.style.textTransform = "capitalize"
+    dd.textContent = selectedChar[attr]
+    dl.appendChild(dt)
+    dl.appendChild(dd)
   })
+  console.log(dl);
+  results.appendChild(dl)
+  results.classList.toggle("fadeIn")
+  results.classList.remove("d-none")
+})
